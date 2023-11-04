f5bc22aa8e4109dc8cc75593fe6462559d0f3d0a
SANGEETHA V D <sangeetha2000.vd@gmail.com>
2023-11-04 20:56:41 +0530

    send email implemented

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const tls = require('tls');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
+// Parses JSON payloads
+app.use(express.json());
+
 app.get('/api/domain-certificate', (req, res) => {
+
   // Get the domain parameter from the query string
   const domain = req.query.domain;
 
@@ -12,7 +17,7 @@ app.get('/api/domain-certificate', (req, res) => {
     return res.status(400).json({ error: 'Missing domain parameter' });
   }
 
-  // Fetch the SSL certificate details
+  // Set the options' values to connect with TLS
   const options = {
     host: domain,
     port: 443,
@@ -22,8 +27,11 @@ app.get('/api/domain-certificate', (req, res) => {
   };
 
   const secureSocket = tls.connect(options, () => {
+
     // Parse the SSL certificate details
     const cert = secureSocket.getPeerCertificate();
+
+    // Fetch the SSL certificate details
     const details = {
       issuer: cert.issuer,
       valid_from: cert.valid_from,
@@ -38,11 +46,47 @@ app.get('/api/domain-certificate', (req, res) => {
     secureSocket.end();
   });
 
+  // Print error message if any error occurs with TLS connection
   secureSocket.on('error', (err) => {
     res.status(500).json({ error: err.message });
   });
+
 });
 
+  
+app.post('/api/send-email', (req, res) => {
+
+  // Set configurations of sender email
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'noreply.expiry.reminder@gmail.com',
+      pass: 'mmva eabh qjni ahgi'
+    },
+  });
+
+  // Get the mail specifications from the request string
+  const to = req.body.to;
+  const domain = req.body.domain;
+  const end_date = req.body.valid_until;
+  const mailOptions = {
+    from: '	noreply.expiry.reminder@gmail.com',
+    to: to,
+    subject: 'Expiring Soon!!!',
+    text: 'This is a reminder email to inform you that your ' + domain + ' certificate expires on ' + end_date + "."
+  };
+
+  // Send out the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.json({ message: 'Error sending email - ' + error});
+    } else {
+      res.json({ message: 'Email sent - ' + info.response});
+    }
+  });  
+});
+
+// Listen to the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
